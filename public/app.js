8ee6fd1b15c87418cf4f11a5ef06f07cfb3fd55b
DocLJN <ljnoble@gmail.com>
2018-02-19 20:59:43 +0000

    images resized

--- public/app.js
+++ public/app.js
@@ -2,19 +2,19 @@ const CATS_ARRAY = [
   { name: "Boba",
     food: "Sock Fluff",
     image: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg",
-    image_width: "500"
+    image_width: "400"
   },
 
   { name: "Barnaby",
     food: "Tuna",
     image: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg",
-    image_width: "500"
+    image_width: "400"
   },
 
   { name: "Max",
     food: "Whiskas Temptations",
     image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg",
-    image_width: "500"
+    image_width: "400"
   },
 ];
 
